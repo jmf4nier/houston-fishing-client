28e48374a1aa238e788290dc6ec0c208fa7cea90
jmf4nier <jmf4nier@gmail.com>
2019-10-05 16:07:31 -0500

    production build

---
+++ build/precache-manifest.d5e694a4e4cce40a2a3d3c450a0c5498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c6566f8c27b3b6abef1b24d546d28c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6426656ae778b7dadfb0",
+    "url": "/static/css/2.7641625d.chunk.css"
+  },
+  {
+    "revision": "f2644be76329eb981c5e",
+    "url": "/static/css/main.1cc7cdbf.chunk.css"
+  },
+  {
+    "revision": "6426656ae778b7dadfb0",
+    "url": "/static/js/2.6867afe8.chunk.js"
+  },
+  {
+    "revision": "f2644be76329eb981c5e",
+    "url": "/static/js/main.13fabd0f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
