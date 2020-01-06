4ace4b1e33a42901cfc9a09ad749559f468bd88a
jmf4nier <jmf4nier@gmail.com>
2020-01-06 14:34:21 -0600

    new branch, new start

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
 import React from "react";
-// import history from "./history";
-// import { Router, Route } from "react-router-dom";
-import "./App.css";
-import LakeIndex from "./components/lakes/lakeIndex";
-import Login from "./components/userForms/login";
-import SignUp from "./components/userForms/signUp";
-// import LakeShow from "./components/lakes/lakeShow";
-// import NavBar from "./components/navBar/navBar";
-import { connect } from "react-redux";
-import { fetchLakes } from "./components/actions/lakeActions";
+import { Provider } from "react-redux";
+import {store} from './store'
 
-class App extends React.Component {
-  componentDidMount() {
-    this.props.fetchLakes();
-    window.scrollTo(0, 0);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        {this.props.showLogin ? <Login /> : null}
-        {this.props.showSignup ? <SignUp /> : null}
-        
-        <LakeIndex />
-      </div>
-    );
-  }
+export default class App extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <div className="App">Hello</div>
+            </Provider>
+        );
+    }
 }
-
-const mapStateToProps = state => ({
-  lake: state.lakeReducer.currentLake,
-  showLogin: state.userReducer.showLogin,
-  showSignup: state.userReducer.showSignup
-});
-export default connect(
-  mapStateToProps,
-  { fetchLakes }
-)(App);
-
-// {/* <Router history={history}>
-//           <NavBar/>
-//           <Route exact path='/' component={LakeIndex}/>
-//         </Router> */}
--- src/store.js
+++ src/store.js
@@ -2,8 +2,6 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./components/reducers";
 
-// const initialState = {};
-
 const middleware = [thunk];
 
 export const store = createStore(
@@ -11,6 +9,6 @@ export const store = createStore(
   undefined,
   compose(
     applyMiddleware(...middleware),
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
